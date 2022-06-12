22a9191f395cb75aa2e8e34ef0fd40c99ec6978c
Nurlan <ikhsan.nurlan@gmail.com>
2022-06-12 14:20:08 +0600

    solved by goroutine

--- cache.go
+++ cache.go
@@ -3,21 +3,42 @@ package cache
 import "time"
 
 type Cache struct {
+	m map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		m: make(map[string]string),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	v, ok := c.m[key]
+	return v, ok
 
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.m[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var ss []string
+	for k, _ := range c.m {
+		ss = append(ss, k)
+	}
+	return ss
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.m[key] = value
+	//c.PutTill("s", "v", time.Now())
+
+	go c.Delete(key, deadline)
+}
+
+func (c *Cache) Delete(k string, deadline time.Time) {
+	d := deadline.Sub(time.Now())
+	time.Sleep(d)
+	delete(c.m, k)
 }
